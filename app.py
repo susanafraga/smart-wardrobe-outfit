fe8ab2d98f5ef1d0dbb940fbd2f716e447c5cd20
susanafraga <202009108@alu.comillas.edu>
2025-12-03 18:13:05 +0100

    deprecated function dash

--- app.py
+++ app.py
@@ -836,4 +836,4 @@ def on_checkout(_):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8050))
-    app.run_server(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port, debug=False)
